8759fa4300b83cd7a4a05f213a9eee1ed3c8c378
YANDIRA SUMA <81127059+Yandira-suma@users.noreply.github.com>
2023-01-23 13:44:53 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado=0;
+        resultado =suma(13,15,17);
+        System.out.println("suma: "+resultado);
+    }
+    public static int suma(int x, int y, int z){
+        return x+y+z;
+    }
+}
